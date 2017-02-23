65110da167d91bbf7bdc122ff8628d7f61488259
Yashwanth <yashwanth.putta@campsystems.com>
2017-02-23 15:26:10 +0530

    Initial Commit with Day 0 challenge

---
+++ 30dayschallenge/0HelloWorld/0HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Printing Hello, World and user input
+/// </summary>
+namespace _0HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = "";
+            inputString = Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(inputString);
+            Console.ReadLine();
+        }
+    }
+}
