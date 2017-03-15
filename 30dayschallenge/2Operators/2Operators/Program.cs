efcda28016002bd6b78d8f2514bbccd9e68e0b13
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-15 14:48:22 +0530

    2-Operators problem solved

---
+++ 30dayschallenge/2Operators/2Operators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal mealcost = decimal.Parse(Console.ReadLine());
+            decimal tipPercent = decimal.Parse(Console.ReadLine());
+            decimal taxPercent = decimal.Parse(Console.ReadLine());
+
+            decimal tip = mealcost * (tipPercent / 100);
+            Console.WriteLine(tip);
+            decimal tax = mealcost * (taxPercent / 100);
+            Console.WriteLine(tax);
+            decimal totalCost = mealcost + tip + tax;
+            Console.WriteLine(totalCost);
+            var totalRoundedCost = Math.Round(totalCost, MidpointRounding.AwayFromZero);
+            Console.WriteLine("The total meal cost is {0} dollars.",totalRoundedCost);
+        }
+    }
+}
