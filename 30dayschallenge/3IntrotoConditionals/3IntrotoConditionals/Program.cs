ab8cead3d2ac18915c0ef06ca37d3d4b163c0a27
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-15 14:54:28 +0530

    3-Intro to conditionals Solved

---
+++ 30dayschallenge/3IntrotoConditionals/3IntrotoConditionals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3IntrotoConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N >=1 && N <=100)
+            {
+                if (N % 2 != 0)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (N % 2 == 0 && N >= 2 && N <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (N >= 6 && N <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (N > 20 && N % 2 == 0)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
+            
+        }
+    }
+}
