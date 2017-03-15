b3b1df9e5affb6d9b76bfa83ba398f4dfc8c8d17
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-15 15:17:17 +0530

    5Loops solved

---
+++ 30dayschallenge/5Loops/5Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n>=2 && n<=20)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}",n,i,n * i);
+                }
+            }
+           
+        }
+    }
+}
