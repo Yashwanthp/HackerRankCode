a2f15938d2a35d8af696c81be418027b412c75e2
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-16 13:20:51 +0530

    7Arrays solved

---
+++ 30dayschallenge/7Arrays/7Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            Array.Reverse(arr);
+            foreach (var a in arr)
+            {
+                Console.Write(a + " ");
+            }
+        }
+    }
+}
