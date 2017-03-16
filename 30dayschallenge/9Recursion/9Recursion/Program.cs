1f35398417ff28a9f0a53160dbb3121fc4518158
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-16 14:13:46 +0530

    9Recursion solved

---
+++ 30dayschallenge/9Recursion/9Recursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factorial(number));
+        }
+
+        public static int factorial(int number)
+        {
+            int x = 1;
+            for (int i = number; i > 1; i = i - 2)
+            {
+                x = x * i * (i - 1);
+            }
+            return x;
+        }
+    }
+}
