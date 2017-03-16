216f52bf733d30279c9ee5a8ad848e271666eff3
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-16 13:52:39 +0530

    8DictionaryandMap solved

---
+++ 30dayschallenge/8DictionaryandMap/8DictionaryandMap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8DictionaryandMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            Dictionary<string,int> dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < numbers; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                dictionary.Add(s[0], int.Parse(s[1]));
+            }
+            for (int i = 0; i < numbers; i++)
+            {
+                string search = Console.ReadLine();
+                if (dictionary.ContainsKey(search))
+                {
+                    Console.WriteLine(search + "=" +dictionary[search]);
+                }
+                else
+                {
+                    Console.WriteLine("Not Found");
+                }
+            }
+        }
+    }
+}
