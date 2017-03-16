ee06b9d0534fd370a0a96f3c0b9733d0fc60e8a9
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-16 13:15:05 +0530

    6LetsReview Solved

---
+++ 30dayschallenge/6LetsReview/6LetsReview/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6LetsReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noofTestCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < noofTestCases; i++)
+            {
+                string s = Console.ReadLine();
+                string evenString = string.Empty;
+                string oddString = string.Empty;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        evenString += s[j];
+                    }
+                    else
+                    {
+                        oddString += s[j];
+                    }
+                }
+                Console.WriteLine(evenString + " " + oddString);
+            }
+        }
+    }
+}
