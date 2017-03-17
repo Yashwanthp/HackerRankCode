46d07884d3d88b322f5a09863a9a3e23f95a2a35
Yashwanth <yashwanth.putta@campsystems.com>
2017-03-17 14:18:54 +0530

    modified code based on resharper recommendations

--- 30dayschallenge/10BinaryNumbers/10BinaryNumbers/Program.cs
+++ 30dayschallenge/10BinaryNumbers/10BinaryNumbers/Program.cs
@@ -20,20 +20,19 @@ namespace _10BinaryNumbers
                     {
                         consecutiveone = thisIterationConsecutiveCount;
                     }
-                    for (int j = i+1; j < binary.Length; j++)
+                    int j;
+                    for (j = i+1; j < binary.Length; j++)
                     {
-                        if (binary[j].ToString() == "1")
+                        if (binary[j].ToString() != "1")
                         {
-                            thisIterationConsecutiveCount++;
-                            if (thisIterationConsecutiveCount > consecutiveone)
-                            {
-                                consecutiveone = thisIterationConsecutiveCount;
-                            }
-                        }
-                        else if (binary[j].ToString() == "0")
-                        {
+                            if (binary[j].ToString() != "0") continue;
                             i = j;
                             break;
+                        }
+                        thisIterationConsecutiveCount++;
+                        if (thisIterationConsecutiveCount > consecutiveone)
+                        {
+                            consecutiveone = thisIterationConsecutiveCount;
                         }
                     }
                 }
